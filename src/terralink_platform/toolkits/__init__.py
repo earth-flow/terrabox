85ce5a8f3932d9d414841f461c3d8a8df306e535
root <xiongzhitong@gmail.com>
2025-08-31 16:17:37 +0900

    remove

--- src/terralink_platform/toolkits/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Built-in toolkits for the Terralink platform.
-
-This package contains built-in toolkit plugins that are automatically
-loaded when the platform starts. Each module in this package should
-implement a setup(registrar) function to register its tools.
-"""
